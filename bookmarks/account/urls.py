79c57f2af605ac7ef8ac8cd7348602c8401e6cb9
abdobassel <abdobassel@gmail.com>
2023-10-05 00:35:38 +0300

    by bassel some errors solved

---
+++ bookmarks/account/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+urlpatterns = [
+    #path('login/', views.user_login, name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    #path('signup/', views.signUp, name='signup'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('register/', views.register, name='register'),
+    path('register/done', views.register, name='register_done'),
+    path('edit/', views.edit, name='edit'),
+
+
+
+
+    path('home/', views.home, name='home'),
+    path('', views.dashboard, name='dashboard'),
+
+
+
+]
